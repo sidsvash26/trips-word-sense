e22d9acbb66c29196476ff4c075a693d3cebda2a
Rik <rbose@cs.rochester.edu>
2019-02-25 17:36:38 -0500

    why not just follow the instructions

--- get_models.sh
+++ get_models.sh
@@ -1,3 +1,3 @@
-mkdir -r models
-wget www.comp.nus.edu.sg/~nlp/sw/IMS_v0.9.2.1.tar.gz -o models/IMS_v0.9.2.1.tar.gz
-wget sterling8.d2.comp.nus.edu.sg/sw/models-MUN-SC-wn30.tar.gz -o models/models-MUN-SC-wn30.tar.gz
+mkdir ims
+wget -qO- www.comp.nus.edu.sg/~nlp/sw/IMS_v0.9.2.1.tar.gz | tar xvz - -C ims
+wget -qO- sterling8.d2.comp.nus.edu.sg/sw/models-MUN-SC-wn30.tar.gz | tar xvz - -C ims
