ae4083343d42f6a5629d2b85a17644a3fb841be6
Rik <rbose@cs.rochester.edu>
2019-02-26 00:54:55 -0500

    oops

--- get_models.sh
+++ get_models.sh
@@ -1,3 +1,7 @@
+IMS_VERSION=0.9.2.1
 mkdir ims
-wget -qO- www.comp.nus.edu.sg/~nlp/sw/IMS_v0.9.2.1.tar.gz | tar xvz - -C ims
+wget -qO- www.comp.nus.edu.sg/~nlp/sw/IMS_v${IMS_VERSION}.tar.gz | tar xvz -
+mv IMS_$IMS_VERSION/testPlain.bash
+chmod +x ims/testPlain.bash
+wget -qO- www.comp.nus.edu.sg/~nlp/sw/lib.tar.gz | tar xvz - -C ims
 wget -qO- sterling8.d2.comp.nus.edu.sg/sw/models-MUN-SC-wn30.tar.gz | tar xvz - -C ims
